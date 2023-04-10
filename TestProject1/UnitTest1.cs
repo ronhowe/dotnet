75aaae3b394b0b111c025ab43814c86fa2343977
Ron Howe <ronhowe@hotmail.com>
2023-04-10 09:55:25 -0400

    Updated UnitTest.cs

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace TestProject1;
 
 [TestClass]
@@ -6,6 +8,7 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
-        Assert.Inconclusive();
+        Trace.WriteLine(DateTime.Now.ToString());
+        Assert.IsTrue(true);
     }
 }
