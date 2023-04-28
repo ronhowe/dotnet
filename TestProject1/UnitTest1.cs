a71fac9e5f884d44f3536c9c895811f0760eec73
Ron Howe <ronhowe@hotmail.com>
2023-04-28 18:41:51 -0400

    Added ClassLibrary1 with Implementation

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,3 +1,4 @@
+using ClassLibrary1;
 using System.Diagnostics;
 
 namespace TestProject1;
@@ -9,6 +10,10 @@ public class UnitTest1
     public void TestMethod1()
     {
         Trace.WriteLine(DateTime.Now.ToString());
-        Assert.IsTrue(true);
+        var class1 = new Class1();
+        Assert.IsTrue(class1.Method1(true));
+        Assert.IsFalse(class1.Method1(false));
+        Assert.IsTrue(class1.Method1(!false));
+        Assert.IsFalse(class1.Method1(!true));
     }
 }
