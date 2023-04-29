b4a266f79a3c4275e9e964ddf614297457565f8d
Ron Howe <ronhowe@hotmail.com>
2023-04-29 10:55:44 -0400

    Added/Updated Integration Tests

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -9,7 +9,99 @@ namespace TestProject1;
 public class UnitTest1
 {
     [TestMethod]
-    public async Task IntegrationTestMethod1()
+    [Ignore]
+    public async Task PublicIntegrationTestMethod1()
+    {
+        using var client = new HttpClient();
+
+        HttpResponseMessage response;
+
+        var endpoint = "TBD";
+
+        response = await client.GetAsync($"https://{endpoint}/");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+
+        response = await client.GetAsync($"https://{endpoint}/?input=true");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+
+        response = await client.GetAsync($"https://{endpoint}/?input=false");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task FrontDoorIntegrationTestMethod1()
+    {
+        using var client = new HttpClient();
+
+        HttpResponseMessage response;
+
+        var endpoint = "TBD";
+
+        response = await client.GetAsync($"https://{endpoint}/");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+
+        response = await client.GetAsync($"https://{endpoint}/?input=true");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+
+        response = await client.GetAsync($"https://{endpoint}/?input=false");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task AppServiceIntegrationTestMethod1()
+    {
+        using var client = new HttpClient();
+
+        HttpResponseMessage response;
+
+        var endpoint = "TBD";
+
+        response = await client.GetAsync($"https://{endpoint}/");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+
+        response = await client.GetAsync($"https://{endpoint}/?input=true");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+
+        response = await client.GetAsync($"https://{endpoint}/?input=false");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task KestrelIntegrationTestMethod1()
+    {
+        using var client = new HttpClient();
+
+        HttpResponseMessage response;
+
+        var endpoint = "localhost:444";
+
+        response = await client.GetAsync($"https://{endpoint}/");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+
+        response = await client.GetAsync($"https://{endpoint}/?input=true");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+
+        response = await client.GetAsync($"https://{endpoint}/?input=false");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    public async Task InProcessIntegrationTestMethod1()
     {
         using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
         using var client = application.CreateClient();
@@ -19,10 +111,6 @@ public class UnitTest1
         response = await client.GetAsync("/");
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
         Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-
-        //response = await client.GetAsync("/?input=");
-        //Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-        //Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
 
         response = await client.GetAsync("/?input=true");
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
@@ -38,6 +126,7 @@ public class UnitTest1
     {
         var class1 = new Class1();
 
+        Assert.IsFalse(class1.Method1(null));
         Assert.IsTrue(class1.Method1(true));
         Assert.IsFalse(class1.Method1(false));
     }
