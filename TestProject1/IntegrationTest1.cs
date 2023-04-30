7c1947d78d19dd9c7ab01917b7e20c8311dca50d
Ron Howe <ronhowe@hotmail.com>
2023-04-30 13:37:13 -0400

    Refactored Integration Tests

---
+++ TestProject1/IntegrationTest1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace TestProject1;
+
+[TestClass]
+public class IntegrationTest1
+{
+    private const string appName = "app-ronhowe-000";
+
+    [TestMethod]
+    [Ignore]
+    public async Task AppServiceNullStatusCode()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task AppServiceNullContent()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/");
+
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task AppServiceTrueStatusCode()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/?input=true");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task AppServiceTrueContent()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/?input=true");
+
+        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task AppServiceFalseStatusCode()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/?input=false");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task AppServiceFalseContent()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/?input=false");
+
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task KestrelNullStatusCode()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync("https://localhost:444/");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task KestrelNullContent()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync("https://localhost:444/");
+
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task KestrelTrueStatusCode()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync("https://localhost:444/?input=true");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task KestrelTrueContent()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync("https://localhost:444/?input=true");
+
+        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task KestrelFalseStatusCode()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync("https://localhost:444/?input=false");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    [Ignore]
+    public async Task KestrelFalseContent()
+    {
+        using var client = new HttpClient();
+        using var response = await client.GetAsync("https://localhost:444/?input=false");
+
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -8,130 +8,7 @@ namespace TestProject1;
 [TestClass]
 public class UnitTest1
 {
-    private const string appName = "app-prod-idso-000";
-
     [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task AppServiceNullStatusCode()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task AppServiceNullContent()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/");
-
-        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task AppServiceTrueStatusCode()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/?input=true");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task AppServiceTrueContent()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/?input=true");
-
-        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task AppServiceFalseStatusCode()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/?input=false");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task AppServiceFalseContent()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync($"https://{appName}.azurewebsites.net/?input=false");
-
-        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task KestrelNullStatusCode()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync("https://localhost:444/");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task KestrelNullContent()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync("https://localhost:444/");
-
-        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task KestrelTrueStatusCode()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync("https://localhost:444/?input=true");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task KestrelTrueContent()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync("https://localhost:444/?input=true");
-
-        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task KestrelFalseStatusCode()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync("https://localhost:444/?input=false");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    [TestCategory("IntegrationTest")]
-    public async Task KestrelFalseContent()
-    {
-        using var client = new HttpClient();
-        using var response = await client.GetAsync("https://localhost:444/?input=false");
-
-        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
-    [TestMethod]
-    [TestCategory("UnitTest")]
     public async Task InProcessNullStatusCode()
     {
         using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
@@ -142,7 +19,6 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [TestCategory("UnitTest")]
     public async Task InProcessNullContent()
     {
         using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
@@ -153,7 +29,6 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [TestCategory("UnitTest")]
     public async Task InProcessTrueStatusCode()
     {
         using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
@@ -164,7 +39,6 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [TestCategory("UnitTest")]
     public async Task InProcessTrueContent()
     {
         using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
@@ -175,7 +49,6 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [TestCategory("UnitTest")]
     public async Task InProcessFalseStatusCode()
     {
         using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
@@ -186,7 +59,6 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [TestCategory("UnitTest")]
     public async Task InProcessFalseContent()
     {
         using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
@@ -197,7 +69,6 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [TestCategory("UnitTest")]
     public void UnitTestNull()
     {
         var class1 = new Class1();
@@ -206,7 +77,6 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [TestCategory("UnitTest")]
     public void UnitTestTrue()
     {
         var class1 = new Class1();
@@ -215,7 +85,6 @@ public class UnitTest1
     }
 
     [TestMethod]
-    [TestCategory("UnitTest")]
     public void UnitTestFalse()
     {
         var class1 = new Class1();
