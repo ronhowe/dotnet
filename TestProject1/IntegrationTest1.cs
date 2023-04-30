b6523442360bd87c1a4977e4de95b3f4996eb44c
Ron Howe <ronhowe@hotmail.com>
2023-04-30 14:49:06 -0400

    Refactored Integration Tests

---
+++ TestProject1/IntegrationTest1.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace TestProject1;
+
+[TestClass]
+public class IntegrationTest1
+{
+    [TestMethod]
+    public async Task InProcessNullStatusCode()
+    {
+        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        using var client = application.CreateClient();
+        using var response = await client.GetAsync("/");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task InProcessNullContent()
+    {
+        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        using var client = application.CreateClient();
+        using var response = await client.GetAsync("/");
+
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    public async Task InProcessTrueStatusCode()
+    {
+        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        using var client = application.CreateClient();
+        using var response = await client.GetAsync("/?input=true");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task InProcessTrueContent()
+    {
+        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        using var client = application.CreateClient();
+        using var response = await client.GetAsync("/?input=true");
+
+        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+
+    [TestMethod]
+    public async Task InProcessFalseStatusCode()
+    {
+        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        using var client = application.CreateClient();
+        using var response = await client.GetAsync("/?input=false");
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [TestMethod]
+    public async Task InProcessFalseContent()
+    {
+        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        using var client = application.CreateClient();
+        using var response = await client.GetAsync("/?input=false");
+
+        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,73 +1,11 @@
 using ClassLibrary1;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Net;
 
 namespace TestProject1;
 
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod]
-    public async Task InProcessNullStatusCode()
-    {
-        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
-        using var client = application.CreateClient();
-        using var response = await client.GetAsync("/");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    public async Task InProcessNullContent()
-    {
-        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
-        using var client = application.CreateClient();
-        using var response = await client.GetAsync("/");
-
-        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
-    [TestMethod]
-    public async Task InProcessTrueStatusCode()
-    {
-        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
-        using var client = application.CreateClient();
-        using var response = await client.GetAsync("/?input=true");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    public async Task InProcessTrueContent()
-    {
-        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
-        using var client = application.CreateClient();
-        using var response = await client.GetAsync("/?input=true");
-
-        Assert.IsTrue(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
-    [TestMethod]
-    public async Task InProcessFalseStatusCode()
-    {
-        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
-        using var client = application.CreateClient();
-        using var response = await client.GetAsync("/?input=false");
-
-        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-    }
-
-    [TestMethod]
-    public async Task InProcessFalseContent()
-    {
-        using var application = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
-        using var client = application.CreateClient();
-        using var response = await client.GetAsync("/?input=false");
-
-        Assert.IsFalse(Boolean.Parse(response.Content.ReadAsStringAsync().Result));
-    }
-
     [TestMethod]
     public void UnitTestNull()
     {
