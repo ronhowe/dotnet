0f65226a2c4f5b3be766264d3565b070f22d2aac
Ron Howe <ronhowe@hotmail.com>
2023-10-09 08:31:50 -0400

    Disabled Live Tests

--- TestProject1/LiveTests.cs
+++ TestProject1/LiveTests.cs
@@ -14,10 +14,10 @@ using System.Net;
 
 namespace TestProject1;
 
-[TestClass]
+//[TestClass]
 public class LiveTests : TestBase
 {
-    [TestMethod]
+    //[TestMethod]
     public async Task ClientConnectsToRonHoweNet()
     {
         var retryPolicy = Policy
@@ -68,7 +68,7 @@ public class LiveTests : TestBase
         Log.ForContext("SourceContext", _sourceContext).Debug(await response.Content.ReadAsStringAsync());
     }
 
-    [TestMethod]
+    //[TestMethod]
     public async Task ClientConnectsToFrontDoor()
     {
         var retryPolicy = Policy
