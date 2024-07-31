d7591e835690d93c0889dd3be06a00adc9c69af6
Ron Howe <ronhowe@hotmail.com>
2024-07-31 09:36:14 -0400

    Removed Pragmas in UnitTestHelpers

--- TestProject1/TestHelpers.cs
+++ TestProject1/TestHelpers.cs
@@ -7,43 +7,38 @@ using Moq;
 
 namespace TestProject1;
 
-//todo - remove pragmas, i have no idea what it means =)
 internal static class TestHelpers
 {
     public static Mock<ILogger<T>> VerifyLogDebug<T>(this Mock<ILogger<T>> logger, string expectedMessage)
     {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-        Func<object, Type, bool> state = (v, t) => v.ToString().CompareTo(expectedMessage) == 0;
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        ArgumentNullException.ThrowIfNull(expectedMessage);
 
-#pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+        Func<object, Type, bool> state = (v, t) => v?.ToString()?.CompareTo(expectedMessage) == 0;
+
         logger.Verify(
             x => x.Log(
                 It.Is<LogLevel>(l => l == LogLevel.Debug),
                 It.IsAny<EventId>(),
                 It.Is<It.IsAnyType>((v, t) => state(v, t)),
                 It.IsAny<Exception>(),
-                It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)));
-#pragma warning restore CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)));
 
         return logger;
     }
 
     public static Mock<ILogger<T>> VerifyLogInformation<T>(this Mock<ILogger<T>> logger, string expectedMessage)
     {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-        Func<object, Type, bool> state = (v, t) => v.ToString().CompareTo(expectedMessage) == 0;
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        ArgumentNullException.ThrowIfNull(expectedMessage);
 
-#pragma warning disable CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+        Func<object, Type, bool> state = (v, t) => v?.ToString()?.CompareTo(expectedMessage) == 0;
+
         logger.Verify(
             x => x.Log(
                 It.Is<LogLevel>(l => l == LogLevel.Information),
                 It.IsAny<EventId>(),
                 It.Is<It.IsAnyType>((v, t) => state(v, t)),
                 It.IsAny<Exception>(),
-                It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)));
-#pragma warning restore CS8620 // Argument cannot be used for parameter due to differences in the nullability of reference types.
+                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)));
 
         return logger;
     }
