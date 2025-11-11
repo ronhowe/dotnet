4d8541369f866ea9366a6fb14679d90f62e7722a
Ron Howe <ronhowe@hotmail.com>
2025-11-11 15:18:58 -0500

    Added MyWebApplication10 (.NET 10)

---
+++ MyWebApplication10/RequestLoggingMiddleware.cs
@@ -0,0 +1,11 @@
+﻿namespace MyWebApplication10;
+
+public class RequestLoggingMiddleware(RequestDelegate next, ILogger<RequestLoggingMiddleware> logger)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        logger.LogInformation("Incoming HTTP Request");
+        await next(context);
+        logger.LogInformation("Outgoing HTTP Response");
+    }
+}
